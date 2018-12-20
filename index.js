ec1c03f2a9d0281238eab2adf747776e51754751
Robert Snyder <r.snyder49@gmail.com>
2018-12-20 02:40:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
-   driver[key] = value;
-   return driver;
+  driver[key] = value;
+  return driver;
 }
